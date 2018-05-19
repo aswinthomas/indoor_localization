3585a412513b658441492d44be54dd765cf328c4
Aswin Thomas <aswinthomas@hopetechnik.com>
2018-05-19 14:03:28 +0800

    fovis launch

--- src/odom_to_marker.cpp
+++ src/odom_to_marker.cpp
@@ -3,12 +3,72 @@
 #include <sensor_msgs/Imu.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 
-ros::Publisher pubOdom, pubGPS, pubPositionEstimate, pubPose;
-visualization_msgs::Marker odomPoints, gpsPoints, positionDisplayPoints, posePoints;
+ros::Publisher pubOdom, pubGPS, pubPositionEstimate, pubPose, pubGt, pubViso;
+visualization_msgs::Marker odomPoints, gpsPoints, positionDisplayPoints, posePoints, gtPoints, laserOdomPoints, visoPoints;
 
-double prevOdomTime, prevGPSTime, prevEstimateTime, prevPoseTime;
+double prevOdomTime, prevGPSTime, prevEstimateTime, prevPoseTime, prevGtTime, prevLaserOdomTime, prevVisoTime;
 double displayInterval=0.2;
+
+void visoCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+  if(fabs(prevVisoTime-ros::Time::now().toSec())<displayInterval) {
+    return;
+  }
+  prevVisoTime = ros::Time::now().toSec();
+  visoPoints.header.frame_id = "/my_frame";
+  visoPoints.header.stamp = ros::Time::now();
+  visoPoints.ns = "points";
+  visoPoints.action = visualization_msgs::Marker::ADD;
+  visoPoints.pose.orientation.w = 1.0;
+  visoPoints.id = 0;
+  visoPoints.type = visualization_msgs::Marker::POINTS;
+  // POINTS markers use x and y scale for width/height respectively
+  visoPoints.scale.x = 0.2;
+  visoPoints.scale.y = 0.2;
+  // Points are blue
+  visoPoints.color.g = 1.0f;
+  visoPoints.color.b = 1.0f;
+  visoPoints.color.a = 1.0;
+
+  geometry_msgs::Point tempPoint;
+  tempPoint.x = msg->pose.pose.position.x;
+  tempPoint.y = msg->pose.pose.position.y;
+  tempPoint.z = msg->pose.pose.position.z;
+  visoPoints.points.push_back(tempPoint);
+
+  pubViso.publish(visoPoints);
+}
+
+void gtCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+  if(fabs(prevGtTime-ros::Time::now().toSec())<displayInterval) {
+    return;
+  }
+  prevGtTime = ros::Time::now().toSec();
+  gtPoints.header.frame_id = "/my_frame";
+  gtPoints.header.stamp = ros::Time::now();
+  gtPoints.ns = "points";
+  gtPoints.action = visualization_msgs::Marker::ADD;
+  gtPoints.pose.orientation.w = 1.0;
+  gtPoints.id = 0;
+  gtPoints.type = visualization_msgs::Marker::POINTS;
+  // POINTS markers use x and y scale for width/height respectively
+  gtPoints.scale.x = 0.2;
+  gtPoints.scale.y = 0.2;
+  // Points are red
+  gtPoints.color.r = 1.0f;
+  gtPoints.color.g = 1.0f;
+  gtPoints.color.b = 1.0f;
+  gtPoints.color.a = 1.0;
+
+  geometry_msgs::Point tempPoint;
+  tempPoint.x = msg->pose.position.x;
+  tempPoint.y = msg->pose.position.y;
+  tempPoint.z = msg->pose.position.z;
+  gtPoints.points.push_back(tempPoint);
+
+  pubGt.publish(gtPoints);
+}
 
 void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
   if(fabs(prevPoseTime-ros::Time::now().toSec())<displayInterval) {
@@ -65,6 +125,35 @@ void estimateCallback(const nav_msgs::Odometry::ConstPtr& msg) {
   positionDisplayPoints.points.push_back(tempPoint);
 
   pubPositionEstimate.publish(positionDisplayPoints);
+}
+
+void laserOdomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  if(fabs(prevLaserOdomTime-ros::Time::now().toSec())<displayInterval) {
+    return;
+  }
+  prevLaserOdomTime = ros::Time::now().toSec();
+  laserOdomPoints.header.frame_id = "/my_frame";
+  laserOdomPoints.header.stamp = ros::Time::now();
+  laserOdomPoints.ns = "points";
+  laserOdomPoints.action = visualization_msgs::Marker::ADD;
+  laserOdomPoints.pose.orientation.w = 1.0;
+  laserOdomPoints.id = 0;
+  laserOdomPoints.type = visualization_msgs::Marker::POINTS;
+  // POINTS markers use x and y scale for width/height respectively
+  laserOdomPoints.scale.x = 0.2;
+  laserOdomPoints.scale.y = 0.2;
+  // Points are red
+  laserOdomPoints.color.r = 1.0f;
+  laserOdomPoints.color.a = 1.0;
+
+  geometry_msgs::Point tempPoint;
+  tempPoint.x = msg->pose.pose.position.x;
+  tempPoint.y = msg->pose.pose.position.y;
+  tempPoint.z = msg->pose.pose.position.z;
+  laserOdomPoints.points.push_back(tempPoint);
+
+  pubPose.publish(laserOdomPoints);
 }
 
 void encoderCallback(const nav_msgs::Odometry::ConstPtr& msg)
@@ -136,18 +225,23 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n("~");
 
-  prevOdomTime= prevGPSTime=prevEstimateTime =prevPoseTime= ros::Time::now().toSec();
+  prevOdomTime= prevGPSTime=prevEstimateTime =prevPoseTime=prevGtTime=prevLaserOdomTime=prevVisoTime= ros::Time::now().toSec();
 
   //odom subscriber
   ros::Subscriber subOdom = n.subscribe("/base_odometry/odom", 1000, encoderCallback);
   ros::Subscriber subEstimate = n.subscribe("/odometry/filtered", 1000, estimateCallback);
   ros::Subscriber subGPS = n.subscribe("/navsat/enu", 1000, gpsCallback);
   ros::Subscriber subPose = n.subscribe("/poseupdate", 1000, poseCallback);
+  ros::Subscriber subGt = n.subscribe("/groundtruth_pose", 1000, gtCallback);
+  ros::Subscriber subLaserOdom = n.subscribe("/laser_odom_rf2o", 1000, laserOdomCallback);
+  ros::Subscriber subViso = n.subscribe("/fovis_stereo_odometer/odometry", 1000, visoCallback);
 
   pubPositionEstimate = n.advertise<visualization_msgs::Marker>("/estimate_position_marker", 10);
   pubOdom = n.advertise<visualization_msgs::Marker>("/odom_marker", 10);
   pubGPS = n.advertise<visualization_msgs::Marker>("/gps_marker", 10);
   pubPose = n.advertise<visualization_msgs::Marker>("/laserodom_marker", 10);
+  pubGt = n.advertise<visualization_msgs::Marker>("/groundtruth_marker", 10);
+  pubViso = n.advertise<visualization_msgs::Marker>("/viso_odom_marker", 10);
 
   ros::spin();
 
